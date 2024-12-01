958ae6707d68365b52f429eb9ff9a188a1ca4d48
geonwoo0 <kjk9226@naver.com>
2024-12-01 20:08:14 +0900

    db추가, cv2로 변경

--- asdff.py
+++ asdff.py
@@ -1,133 +1,144 @@
-import time
-import serial
-import requests
-import numpy
-from io import BytesIO
-from pprint import pprint
-from requests.auth import HTTPBasicAuth
-import numpy as np
-
 import cv2
+from io import BytesIO
+import requests
+import numpy as np
+from PIL import Image
+from requests.auth import HTTPBasicAuth
+import serial
+import time
+import sqlite3
+import uuid
+from datetime import datetime
 
 ser = serial.Serial("/dev/ttyACM0", 9600)
+VISION_API_URL = "http://192.168.10.13:8883/inference/run"
 
-# API endpoint
-URL = "http://192.168.10.13:8883/inference/run"
+conn = sqlite3.connect('check_pi.db')
+cursor = conn.cursor()
 
-def get_img():
-    """Get Image From USB Camera
 
-    Returns:
-        numpy.array: Image numpy array
-    """
+def request_image(image):
+    image = np.array(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    cam = cv2.VideoCapture(0)
-
-    if not cam.isOpened():
-        print("Camera Error")
-        exit(-1)
-
-    ret, img = cam.read()
-
-    return img
-
-def inference_reqeust(image: numpy.array):
-    """_summary_
-
-    Args:
-        img (numpy.array): Image numpy array
-        api_rul (str): API URL. Inference Endpoint
-    """
     _, img_encoded = cv2.imencode(".jpg", image)   
-    img_binary = img_encoded.tobytes()
-    img_bytes = BytesIO(img_binary) 
+    img_bytes = BytesIO(img_encoded.tobytes()) 
     files = {"file": ("image.jpg", img_bytes, "image/jpeg")}
 
-
-    """response = requests.post(
-        url=URL,
-        auth=HTTPBasicAuth("kdt2024_1-13", ACCESS_KEY),
-        headers={"Content-Type": "image/jpeg"},
-        data=image,
-    )"""
-    try:
-        response = requests.post(
-        url=URL,
-        #auth=HTTPBasicAuth(TEAM, ACCESS_KEY),
-        #headers={"Content-Type": "image/jpeg"},
+    response = requests.post(
+        url = VISION_API_URL,
         files = files
-    )   
-        if response.status_code == 200:
-            pprint(response.json())
-            return response.json()
-            print("Image sent successfully")
-        else:
-            print(f"Failed to send image. Status : {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error sending request: {e}")
+    )
+    print("상태:",response.status_code)
+    img_obj = response.json()["objects"]
+    return img_obj
 
+def process_image(img_obj,frame):
+    image = np.array(frame)
+    image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    img_cnt = {"a":0,"b":0,"c":0,"d":0,"e":0,"f":0}
+    for obj in img_obj:
+        cls_number = obj["class_number"]
+        confidence = obj["confidence"]
+        bbox = obj["bbox"]
+        if cls_number == 1:
+            cls_name = "a"
+            img_cnt[cls_name] +=1
+            color = (127,255,127)
+        elif cls_number == 2:
+            cls_name = "b"
+            img_cnt[cls_name] +=1
+            color = (255,0,0)
+        elif cls_number == 3:
+            cls_name = "c"
+            img_cnt[cls_name] +=1
+            color = (0,255,0)
+        elif cls_number == 4:
+            cls_name = "d"
+            img_cnt[cls_name] +=1
+            color = (0,0,255)
+        elif cls_number == 5:
+            cls_name = "e"
+            img_cnt[cls_name] +=1
+            color = (255,255,0)
+        elif cls_number == 6:
+            cls_name = "f"
+            img_cnt[cls_name] +=1
+            color = (30,60,90)
+
+        a1,a2,b1,b2 = bbox
+        cv2.rectangle(image, (int(a1),int(a2)), (int(b1),int(b2)), color = color, 2)
+
+        text = f"{cls_name}: {float(confidence):.2f}"
+
+        cv2.putText(image, text, (int(a1), int(a2) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color = color, 2)
+    print("box 생성")
+    k=30
+    for j in img_cnt:
+        text = j + ":" + str(img_cnt[j])
+        k=k+30
+        cv2.putText(image,text,(30,k),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    print("박스 수량")
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    return image
+
+def insert_data(datetime_value, uuid_value, is_defective, defect_reason=None,img_path):
+    insert_query = '''
+    INSERT INTO 제품 (datetime, uuid, is_defective, defect_reason, image_path)
+    VALUES (?, ?, ?, ?, ?)
+    '''
+    cursor.execute(insert_query, (datetime_value, uuid_value, is_defective, defect_reason,img_path))
+    conn.commit()
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # 프레임 폭 줄이기
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # 프레임 높이 줄이기
+cap.set(cv2.CAP_PROP_FPS, 15)  # 초당 프레임 수 제한
 
 while 1:
-    data = ser.read()
-    print(data)
-    img = get_img()
-    cv2.imshow("img",img)
-    if data == b"0":
-        img = get_img()
-        # crop_info = None
-        result = inference_reqeust(img)
-        objects = result["objects"].copy()
-        img = np.array(img)
-        thickness = 2 # 박스 선의 두께
-        count = {"0":0, "1":0,"2":0,"3":0,"4":0,"5":0}
+    _, frame = cap.read()
+    
+    img_obj = request_image(frame)
+    obj_cnt = len(img_obj)
 
-        for i in objects:
-            box = i["bbox"]
-            start_point=(int(box[0]),int(box[1]))
-            end_point=(int(box[2]),int(box[3]))
-            if i["class_number"] == "0":
-                count[i["class_number"]] += 1
-                color = (127,255,127)
-            elif i["class_number"] == "1":
-                color = (255,0,0)
-                count[i["class_number"]] += 1
-            elif i["class_number"] == "2":
-                color = (0,255,0)
-                count[i["class_number"]] += 1
-            elif i["class_number"] == "3":
-                color = (0,0,255)
-                count[i["class_number"]] += 1
-            elif i["class_number"] == "4":
-                color = (255,255,0)
-                count[i["class_number"]] += 1
-            elif i["class_number"] == "5":
-                color = (30,60,90)
-                count[i["class_number"]] += 1
-            else:
-                color = (0,0,0)
-
-            cv2.rectangle(img,start_point,end_point,color = color, thickness = thickness)
-            cv2.putText(img,i["class_number"],start_point,cv2.FONT_HERSHEY_SIMPLEX, 1, color=color,thickness=1)
-        text = ""
-        k=30
-        for j in count.keys():
-            text = str(j) + " : " +str(count[j])
-            k=k+30
-            cv2.putText(img,text,(30,k),cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        cv2.imshow("img_check",img)
-        obj_count = 0
-        for i in count.values():
-            obj_count += i
-        if obj_count == 9:
+    if obj_cnt >= 1:
+        frame = process_image(img_obj,frame)
+        cv2.imshow("image",frame)
+        cv2.waitKey(1)
+        data = ser.read()
+        
+        if data == b"0" and obj_cnt == 9:
+            datetime_value = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            uuid_value = str(uuid.uuid4())
+            is_defective = 0
+            defect_reason = "정상"
+            #이미지 경로 수정
+            cv2.imwrite(f"{uuid_value}{defect_reason}.jpg",frame)
+            image_path = f"{uuid_value}{defect_reason}.jpg"
+            insert_data(datetime_value,uuid_value,is_defective,defect_reason,image_path)
             ser.write(b"1")
-        else:
+
+        elif data == b"0" and obj_cnt < 9:
+            datetime_value = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            uuid_value = str(uuid.uuid4())
+            is_defective = 1
+            defect_reason = "불량"
+            #이미지 경로 수정
+            cv2.imwrite(f"{uuid_value}{defect_reason}",frame)
+            image_path = f"{uuid_value}{defect_reason}"
+            insert_data(datetime_value,uuid_value,is_defective,defect_reason,image_path)
+            cv2.imshow("defect",frame)
+            cv2.waitKey(1)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 ser.write(b"1")
                 pass
-        
-    else:
-        pass
 
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+
+        else:
+            pass
+    else:
+        cv2.imshow("image",frame)
+        cv2.waitKey(1)
